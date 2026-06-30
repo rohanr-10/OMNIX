8e836695a9315912d2ff83a3d9d44aaa27367ae2
Rohan <rohanr24332@gmail.com>
2026-07-01 01:30:59 +0530

    feat: initial working OMNIX app with AI Council foundation

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_omnix_app_OmnixApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.omnix.app.OmnixApplication"
-)
-public final class _com_omnix_app_OmnixApplication {
-}
